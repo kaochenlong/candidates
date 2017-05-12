1d34e6abe8ba8312b5c04c2b7f187503ca35bdf8
Eddie Kao <eddie@digik.com.tw>
2017-05-12 21:31:23 +0800

    add create

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,6 +6,21 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      redirect_to candidates_path
+    else
+      # ....
+    end
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
   end
 end
 
